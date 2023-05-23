2e150ee118e2b821d2cbd0b2de016e6e787a6d74
swati <s.swati.5293@gmail.com>
2023-05-23 16:09:08 +0530

    23rd May'23 commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts'); //requrining ejs layouts for partials and layouts
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts); //saying out app to use expressLayouts 
+
+//if there are any script tags and style tages within subpages(ie. non layout pages/header/footer), then whle rendering it'll be put into layout
+app.set('layout extractStyles', true); 
+app.set('layout extractScripts', true);
+
+app.use('/', require('./routes')); //setting up routess
+
+app.set('view engine', 'ejs'); //setting up view engine
+app.set('views', './views');
+
+
+
+
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Error in connecting to the server!');
+    }
+    console.log(`Server running successfully at port: ${port}`);
+});
