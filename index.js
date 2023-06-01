fcf18eadae24f17335a6664e9fe18842163d6140
swati <s.swati.5293@gmail.com>
2023-06-01 15:46:29 +0530

    1st Jun'23

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts'); //requrining ejs layouts for partials and layouts
 const db = require('./config/mongoose');
 
+const session = require('express-session'); // express-session is a lib used to encrypt user id into cookies
+const passport = require('passport'); //a middleware and lib used for authetication purposes
+const passportLocal = require('./config/passport-local-strategy'); //an added module(strategy) which serves specific purpose in our app
+
+const MongoStore= require('connect-mongo'); //used to store the session cookie in the db so that everytime the server refreshes the user need nt sign in again and again
+
 app.use(express.urlencoded()); //to read post requests where data is sent through url
 app.use(cookieParser());
 
@@ -16,15 +22,36 @@ app.use(expressLayouts); //saying out app to use expressLayouts
 app.set('layout extractStyles', true); 
 app.set('layout extractScripts', true);
 
-app.use('/', require('./routes/users')); //setting up routess
-
 app.set('view engine', 'ejs'); //setting up view engine
 app.set('views', './views');
 
 
+//a middleware that takes in the session cookie and encrytps it
+app.use(session({
+    name: 'codeialll',
+    //todo - change the secret key before deployment before production
+    secret: 'randomsecretkey',
+    saveUninitialized: false, //when the user data/identity is not established we would not require to store that data, hence it is set to false 
+    resave: false, //resave prevents the user data present in the session to cookie to read-write again if there is no change in the data
+    cookie: {
+        maxAge: (1000*60*100) //this is in milliseconds which sums upto be the no. minutes => 1 sec = 1000 ms | 1 min = 60 sec | 100 = minutes
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://localhost/Codeialll',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect mongodb setup ok');
+        }
+    )
+}));
+app.use(passport.initialize()); //tells app to use initialise method in passport
+app.use(passport.session()); //tells app to use session method in passport
 
+app.use(passport.setAutheticatedUser);//setting the current user usage
 
-
+app.use('/', require('./routes/users')); //setting up routess
 
 
 app.listen(port, function(err){
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport'); 
 
 const usersController = require('../controllers/users_controller');
 const homeController = require('../controllers/home_controller');
@@ -10,7 +11,14 @@ router.get('/sign_up', usersController.signUp);
 router.get('/sign_in', usersController.signIn);
 
 router.post('/create', usersController.create);
-router.post('/create-session', usersController.createSession);
-router.get('/profile', usersController.profile);
+// router.post('/create-session', usersController.createSession); //this is for manual authetication
+
+//use passport as a middleware to autheticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/sign_in'}
+) , usersController.createSession);
+
+router.get('/profile', passport.checkAuthetication ,usersController.profile);
 
 module.exports = router;
